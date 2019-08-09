2e1b50b961cb8cd9b6cfa4f0c3cae53977ce0005
imranrrr <emranlateef786@gmail.com>
2019-08-09 15:11:10 +0500

    many_to_many_complete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
 
 
   def index
-
+    @books=Book.all
   end
 
   def new
@@ -14,9 +14,16 @@ class BooksController < ApplicationController
     if @book.save!
       Accout.create!(book_id: @book.id, author_id: current_author.id)
       flash[:notice] = "successfully Added!"
+      redirect_to accouts_path
     else
       flash[:notice] = "Book not Added!"
     end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:bookName)
   end
 
 end
